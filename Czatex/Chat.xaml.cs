23abf2af855dd455b4e0ebba3aa8ebe564da916e
Natalia Kaczor <Natalia Kaczor>
2016-01-08 19:28:02 +0100

    Create login view and chat view

--- Czatex/Chat.xaml.cs
+++ Czatex/Chat.xaml.cs
@@ -10,31 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace Czatex
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Chat : Window
     {
-        public MainWindow()
+        public Chat()
         {
             InitializeComponent();
-        }
-
-        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
-        {
-
-        }
-
-        private void button_Click(object sender, RoutedEventArgs e)
-        {
-            var chat = new Chat();
-            chat.Show();
-            this.Close();
         }
     }
 }
